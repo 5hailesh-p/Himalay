757b429e2cf00e9465b9307194ce45585250e086
AOC <angelofcode01@gmail.com>
2025-09-11 12:23:27 +0530

    theme and login set

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 
 import './App.css'
 import Login from './pages/Login'
@@ -8,28 +8,38 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+const AppContext = createContext();
+export const useAppContext = () => useContext(AppContext);
+
 
 
 function App() {
-  const [login, setLogin] = useState(false)
+  const [mode, setMode] = useState(true);
+  const [login, setLogin] = useState(false);
+
+  const toggleMode = () => setMode((prev) => !prev);
+  const value = { mode, toggleMode, login };
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode ? 'dark' : 'light',
+    },
+  });
 
   return (
     <BrowserRouter>
-      <ThemeProvider theme={darkTheme}>
-        <CssBaseline />
-        <Routes>
-          <Route path="/" element={login?<Login /> : <Home/>} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/sign-in" element={<Login />} />
-        </Routes>
-      </ThemeProvider>
+      <AppContext.Provider value={value}>
+        <ThemeProvider theme={darkTheme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={login ? <Login /> : <Home />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<Login />} />
+          </Routes>
+        </ThemeProvider>
+      </AppContext.Provider>
     </BrowserRouter>
-     
+
+
   )
 }
 
