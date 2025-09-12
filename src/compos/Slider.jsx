be40a39eba9bd49866b16e88da4b3fa0fa79d3cb
AOC <angelofcode01@gmail.com>
2025-09-12 11:29:05 +0530

    slider modified

--- src/compos/Slider.jsx
+++ src/compos/Slider.jsx
@@ -1,23 +1,38 @@
-import { Card, CardContent, Typography } from '@mui/material'
 import React from 'react'
-import Swiper from 'swiper'
-import { SwiperSlide } from 'swiper/react'
+import { Box } from '@mui/material'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Autoplay } from 'swiper/modules'
+import 'swiper/css'
 
 const Slider = () => {
+  const img = [
+    'https://images.unsplash.com/photo-1631139946993-b29d3b5c00a9?q=80&w=923&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://images.unsplash.com/photo-1652216080847-d9a517b86134?q=80&w=892&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  ]
+
   return (
-    <>
-    <Swiper spaceBetween={10} slidesPerView={1}>
-      {[1,2,3].map((item)=>(
-        <SwiperSlide key={item}>
-          <Card>
-            <CardContent>
-              <Typography variant='h4'>item {item}</Typography>
-            </CardContent>
-          </Card>
+    <Swiper
+      spaceBetween={10}
+      slidesPerView={1}
+      autoplay={{ delay: 2000, disableOnInteraction: false }}
+      modules={[Autoplay]}
+      style={{ height: '400px' }} // 👈 control slider height
+    >
+      {img.map((item, index) => (
+        <SwiperSlide key={index}>
+          <Box
+            component="img"
+            src={item}
+            sx={{
+              height: '400px',
+              width: '100%',
+              objectFit: 'cover',
+              borderRadius: 2,
+            }}
+          />
         </SwiperSlide>
       ))}
     </Swiper>
-    </>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../compos/Header'
 import Footer from '../compos/Footer'
+import Slider from '../compos/Slider'
 
 const Home = () => {
   return (
     <>
         <Header/>
+        <Slider/>
         <Footer/>
     </>
   )
