2a0039a5239669fcba81bc8d7503fbdafebb889a
5hailesh-p <shailesh.p2701@gmail.com>
2025-09-18 08:47:38 +0530

    ourservices added

--- src/compos/Services.jsx
+++ src/compos/Services.jsx
@@ -6,39 +6,72 @@ import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
 import { Box, Grid } from '@mui/material';
 const Services = () => {
+    const ourServices = [
+        {
+            title: "webdesign",
+            imgPath: 'https://picsum.photos/450/160',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit natus soluta, ab voluptatem ea!',
+        },
+        {
+            title: "appdesign",
+            imgPath: 'https://picsum.photos/450/160',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit natus soluta, ab voluptatem ea!',
+        },
+        {
+            title: "destopapp",
+            imgPath: 'https://picsum.photos/450/160',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit natus soluta, ab voluptatem ea!',
+        },
+        {
+            title: "destopapp",
+            imgPath: 'https://picsum.photos/450/160',
+            desc: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo impedit natus soluta, ab voluptatem ea!',
+        },
+    ]
     return (
         <>
-        <Box container sx={{textAlign:'center',marginBottom:'4rem'}}  >
-            <Typography variant='h2'>Our Services</Typography>
-            <Typography variant='h6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, eum sunt dolorem autem eius facere.</Typography>
-        </Box>
-            <Grid container  sx={{margin:"auto"}} maxWidth="lg" spacing={2}>
-                
-                <Grid  size={{xs:12,md:4}} >
-                    <Card  >
-                        <CardActionArea>
-                            <CardMedia
-                                component="img"
-                                height="140"
-                                image="/static/images/cards/contemplative-reptile.jpg"
-                                alt="green iguana"
-                            />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    Lizard
-                                </Typography>
-                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                    Lizards are a widespread group of squamate reptiles, with over 6,000
-                                    species, ranging across all continents except Antarctica
-                                </Typography>
-                            </CardContent>
-                        </CardActionArea>
-                    </Card>
-                </Grid>
-                
+            <Box sx={{ textAlign: 'center', marginBottom: '4rem' }}  >
+                <Typography variant='h2'>Our Services</Typography>
+                <Typography variant='h6'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem, eum sunt dolorem autem eius facere.</Typography>
+            </Box>
+            <Grid container sx={{ margin: "auto" }} maxWidth="lg" spacing={2}>
+                {ourServices.map((items, index) => {
+                   return <ServicesCard key={index} items={items} />
+
+                })}
+
             </Grid>
         </>
     )
 }
+
+
+
+export const ServicesCard = ({items}) => {
+    return (
+
+        <Grid size={{ xs: 12, md: 4 }} >
+            <Card  >
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="140"
+                        image={items.imgPath}
+                        alt={items.title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                           {items.title}
+                        </Typography>
+                        <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                            {items.desc}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+    )
+}
+
 
 export default Services
