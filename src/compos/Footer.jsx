8cfd873b635e4039326172c7acdd9f223bd39088
5hailesh-p <shailesh.p2701@gmail.com>
2025-09-19 07:53:54 +0530

    links set to footer by env

--- src/compos/Footer.jsx
+++ src/compos/Footer.jsx
@@ -5,8 +5,14 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
+import LandscapeIcon from '@mui/icons-material/Landscape';
 
 const Footer = () => {
+  const telegramLink = import.meta.env.VITE_SOCIAL_TELEGRAM;
+  const githubLink = import.meta.env.VITE_SOCIAL_GITHUB;
+  const facebookLink = import.meta.env.VITE_SOCIAL_FACEBOOK;
+  const instagramLink = import.meta.env.VITE_SOCIAL_INSTAGRAM;
+  const youtubeLink = import.meta.env.VITE_SOCIAL_YOUTUBE;
   return (
     <Box
       sx={{
@@ -20,16 +26,17 @@ const Footer = () => {
       <Container maxWidth="lg">
         <Grid container spacing={4}>
           {/* Logo & Text */}
-          <Grid   size={{ xs: 12, md: 6}}>
+          <Grid size={{ xs: 12, md: 6 }}>
             <Box display="flex" alignItems="center" mb={1}>
-              <Box
+              <LandscapeIcon   />
+              {/* <Box
                 component="img"
                 src="https://via.placeholder.com/40"
                 alt="logo"
                 sx={{ mr: 1, borderRadius: "50%" }}
-              />
+              /> */}
               <Typography variant="h6" fontWeight="bold">
-                MyCompany
+                {import.meta.env.VITE_SITE_NAME}
               </Typography>
             </Box>
             <Typography variant="body2">
@@ -38,7 +45,7 @@ const Footer = () => {
           </Grid>
 
           {/* Quick Links */}
-          <Grid   size={{ xs: 12, md: 3 }}>
+          <Grid size={{ xs: 12, md: 3 }}>
             <Typography variant="h6" gutterBottom>
               Quick Links
             </Typography>
@@ -51,24 +58,24 @@ const Footer = () => {
           </Grid>
 
           {/* Social Media */}
-          <Grid   size={{ xs: 12, md: 3 }}>
+          <Grid size={{ xs: 12, md: 3 }}>
             <Typography variant="h6" gutterBottom>
               Follow Us
             </Typography>
             <Box>
-              <IconButton color="inherit" href="#">
+              <IconButton color="inherit" href={telegramLink} target="_blank">
                 <TelegramIcon />
               </IconButton>
-              <IconButton color="inherit" href="#">
+              <IconButton color="inherit" href={githubLink} target="_blank">
                 <GitHubIcon />
               </IconButton>
-              <IconButton color="inherit" href="#">
+              <IconButton color="inherit" href={facebookLink} target="_blank">
                 <FacebookIcon />
               </IconButton>
-              <IconButton color="inherit" href="#">
+              <IconButton color="inherit" href={instagramLink} target="_blank">
                 <InstagramIcon />
               </IconButton>
-              <IconButton color="inherit" href="#">
+              <IconButton color="inherit" href={youtubeLink} target="_blank">
                 <YouTubeIcon />
               </IconButton>
             </Box>
@@ -78,7 +85,7 @@ const Footer = () => {
         {/* Divider */}
         <Box mt={3} borderTop="1px solid rgba(255,255,255,0.2)" pt={2}>
           <Typography variant="body2" align="center">
-            © {new Date().getFullYear()} <Typography  variant="span"  ><b>{import.meta.env.VITE_SITE_NAME}</b></Typography> . All rights reserved.
+            © {new Date().getFullYear()} <Typography variant="span"  ><b>{import.meta.env.VITE_SITE_NAME}</b></Typography> . All rights reserved.
           </Typography>
         </Box>
       </Container>
